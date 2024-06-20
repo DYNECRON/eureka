7bb87da354aa71f5ce02f271236b968cc4e6f009
Afkerian <alejandro144ago@gmail.com>
2024-06-19 22:39:01 -0500

    feat: refactor code

--- backend/app/modules/load_data.py
+++ backend/app/modules/load_data.py
@@ -5,31 +5,27 @@ import pandas as pd
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import string
-from collections import defaultdict
 
-stop_words_paht = 'data/reuters/stopwords'
+stop_words_path = 'data/reuters/stopwords'
 data_directory = 'data/reuters/training/'
 filenames = os.listdir(data_directory)
 filenames.sort(key=lambda x: int(x))
 lemmatizer = WordNetLemmatizer()
-word_counts = defaultdict(int)
-wset = set()
 nltk.download('stopwords')
-stop_words2 = set(stopwords.words('english'))
-stem_docs = {}
+stop_words_nltk = set(stopwords.words('english'))
+processed_doc = {}
 real_doc = {}
 stop_words = []
 
 
 def normalize_text(text):
     text = text.lower()
-    text = re.sub(r'[^\w\s]', '', text)
     text = re.sub(r'[^a-zA-Z\s]', '', text)
     text = re.sub(r'\s+', ' ', text).strip()
     return text
 
 
-with open(stop_words_paht, 'r') as file:
+with open(stop_words_path, 'r') as file:
     content = file.read().split('\n')
     stop_words.append(content)
 
@@ -40,34 +36,31 @@ def load_data():
             content = file.read()
             real_doc[filename] = content
             content = normalize_text(content)
-            sdoc = []
-            for word in content.lower().translate(str.maketrans('', '', string.punctuation)).split(" "):
-                if word not in stop_words[0] or word not in stop_words2:
+            words_list = []
+            for word in content.translate(str.maketrans('', '', string.punctuation)).split(" "):
+                if word not in stop_words[0] or word not in stop_words_nltk:
                     word = lemmatizer.lemmatize(word)
-                    if word not in stop_words[0] or word not in stop_words2:
-                        wset.add(word)
-                        word_counts[word] += 1
-                        sdoc.append(word)
-            sdoc_concatenates = " ".join(sdoc)
-            stem_docs[filename] = sdoc_concatenates
+                    words_list.append(word)
+            lemmatized_doc = " ".join(words_list)
+            processed_doc[filename] = lemmatized_doc
 
 
 def load_data_to_dataframe():
     load_data()
     filenames_list = []
     original_text_list = []
-    stemmed_text_list = []
+    processed_text = []
 
     # Iterar sobre cada nombre de archivo y sus correspondientes textos originales y procesados
     for filename, original_text in real_doc.items():
         filenames_list.append(filename)
         original_text_list.append(original_text)
-        stemmed_text_list.append(stem_docs[filename])
+        processed_text.append(processed_doc[filename])
 
     # Crear el DataFrame
     df = pd.DataFrame({
         'filename': filenames_list,
         'original_text': original_text_list,
-        'stemmed_text': stemmed_text_list
+        'processed_text': processed_text
     })
     return df
